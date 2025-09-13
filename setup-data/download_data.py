9ee58ca369a8272eac599d752ff708b0c3b570e2
AleG19 <62238588+monteleone-1883922@users.noreply.github.com>
2025-09-13 18:51:27 +0200

    Implement directory creation for databases
    Added a function to create data directories for multiple databases.

--- setup-data/download_data.py
+++ setup-data/download_data.py
@@ -1,8 +1,16 @@
 from retrieve_form_rest_api import setup
+from pathlib import Path
 
+DIR_TO_CREATE = "../{database}/data/"
+DATABASES = ["neo4j", "cassandra", "elasticsearch", "mongodb", "postgres", "metabase"]
 
+def create_directories():
+    for database in DATABASES:
+        Path(DIR_TO_CREATE.format(database=database)).mkdir()
+    print("Data directories created")
 
 if __name__ == '__main__':
+    create_directories()
     setup('tornado')
     setup('county')
     setup('volcano')
